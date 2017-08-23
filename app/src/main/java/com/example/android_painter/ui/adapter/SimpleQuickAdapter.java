c8628299fac8ac16099d58e72c4a8466e22715fa
ZhangXinmin528 <zhangxinmin528@sina.com>
2017-08-23 22:07:11 +0800

    完善探索界面

--- app/src/main/java/com/example/android_painter/ui/adapter/SimpleQuickAdapter.java
+++ app/src/main/java/com/example/android_painter/ui/adapter/SimpleQuickAdapter.java
@@ -1,10 +1,10 @@
 package com.example.android_painter.ui.adapter;
 
-import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.android_painter.R;
 import com.example.android_painter.model.ExploreInfo;
 
 import java.util.List;
@@ -12,17 +12,19 @@ import java.util.List;
 /**
  * Created by ZhangXinmin on 2017/8/23.
  * Copyright (c) 2017 . All rights reserved.
- * 简单适配器
+ * 探索页面适配器
  */
 
-public class SimpleQuickAdapter extends BaseQuickAdapter<ExploreInfo,BaseViewHolder> {
+public class SimpleQuickAdapter extends BaseQuickAdapter<ExploreInfo, BaseViewHolder> {
 
-    public SimpleQuickAdapter(@LayoutRes int layoutResId, @Nullable List<ExploreInfo> data) {
-        super(layoutResId, data);
+    public SimpleQuickAdapter(@Nullable List<ExploreInfo> data) {
+        super(R.layout.layout_explore_item, data);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, ExploreInfo item) {
-
+        helper.setText(R.id.tv_title, item.getTitle())
+                .setImageResource(R.id.iv_pic, item.getResId())
+                .setText(R.id.tv_desc, item.getDesc());
     }
 }
--- app/src/main/java/com/example/android_painter/ui/fragment/ExploreFragment.java
+++ app/src/main/java/com/example/android_painter/ui/fragment/ExploreFragment.java
@@ -2,14 +2,20 @@ package com.example.android_painter.ui.fragment;
 
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.android_painter.R;
 import com.example.android_painter.model.ExploreInfo;
+import com.example.android_painter.ui.adapter.SimpleQuickAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +26,14 @@ import java.util.List;
  * 探索界面
  */
 
-public class ExploreFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
+public class ExploreFragment extends BaseFragment implements
+        SwipeRefreshLayout.OnRefreshListener, BaseQuickAdapter.OnItemClickListener {
 
     private static final String TAG = ExploreFragment.class.getSimpleName();
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
+    private SimpleQuickAdapter mSimpleQuickAdapter;
     private Handler mHandler;
 
     public static ExploreFragment newInstance() {
@@ -45,12 +53,27 @@ public class ExploreFragment extends BaseFragment implements SwipeRefreshLayout.
 
     @Override
     void initViews(View rootView) {
+        //init actionbar
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        ActionBar actionBar = activity.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.all_explore);
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
         mSwipeRefreshLayout = rootView.findViewById(R.id.swipe_layout);
         //设置颜色
         mSwipeRefreshLayout.setColorSchemeResources(R.color.colorLightBlue, R.color.colorCycn,
                 R.color.colorPurple);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
         mRecyclerView = rootView.findViewById(R.id.recyclerview_explore);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         //init adapter
+        mSimpleQuickAdapter = new SimpleQuickAdapter(initExploreData());
+        mRecyclerView.setAdapter(mSimpleQuickAdapter);
+        mSimpleQuickAdapter.setOnItemClickListener(this);//点击事件
+        mSimpleQuickAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_LEFT);
     }
 
 
@@ -74,9 +97,9 @@ public class ExploreFragment extends BaseFragment implements SwipeRefreshLayout.
     private List<ExploreInfo> initExploreData() {
         List<ExploreInfo> list = new ArrayList<>();
         //常规绘制
-        ExploreInfo common = new ExploreInfo("常规绘制", getString(R.string.desc_common), R.drawable.ic_drawer);
+        ExploreInfo common = new ExploreInfo("常规绘制", getString(R.string.desc_common), R.drawable.click_head_img_0);
         //裁剪绘制
-        ExploreInfo clip = new ExploreInfo("范围裁切", getString(R.string.desc_clip), R.drawable.ic_clip);
+        ExploreInfo clip = new ExploreInfo("范围裁切", getString(R.string.desc_clip), R.drawable.click_head_img_1);
         list.add(common);
         list.add(clip);
         return list;
@@ -85,5 +108,10 @@ public class ExploreFragment extends BaseFragment implements SwipeRefreshLayout.
     @Override
     public void onRefresh() {
 
+    }
+
+    @Override
+    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+        Toast.makeText(mContext, " 点击了：" + position, Toast.LENGTH_SHORT).show();
     }
 }
